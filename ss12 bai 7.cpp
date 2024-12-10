3544b43294b638b81009a4fa59ef5c69f407d2d5
ChloeAmelinda <trangthichngunuong@gmail.com>
2024-12-10 23:37:08 +0700

    ss12 bai 7

---
+++ ss12 bai 7.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void khai_bao(int arr[100][100] , int *n,int *m){ // int *n ,*m la tham so con tro  
+	printf("Nhap so cot :");
+	scanf("%d", n);
+	printf("Nhap so hang:");
+	scanf("%d", m);
+	for(int i=0; i< *n; i++){
+		for(int j=0; j< *m ;j++){
+			printf("Nhap phan tu thu [%d][%d]: ",i+1,j+1);
+			scanf("%d",&arr[i][j]);
+		}
+	}
+}
+
+int main(){
+	int n,m;
+	int arr[100][100];
+	khai_bao(arr,&n,&m);
+	printf("mang da nhap la :\n");
+	for(int i=0; i<n ; i++){
+		for(int j=0; j<m ; j++){
+			printf("%d\t",arr[i][j]);
+		}
+		printf("\n"); 
+	}
+	return 0;
+}
